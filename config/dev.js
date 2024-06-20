20fa5dd09be050307f2e5c3fed2502d4310bb795
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2024-06-20 22:43:33 +1000

    finished auth flow

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+// This is dev.js, don't commit this!
+module.exports = {
+    googleClientID: '566698394086-o66qackhh86cq8ptl9b80uua27onjant.apps.googleusercontent.com',
+    googleClientSecret: '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',
+    mongoURI: 'mongodb+srv://sylvia501628:yGSrU5fO1LODcD8D@cluster0.awmootv.mongodb.net/emailydb?retryWrites=true&w=majority&appName=Cluster0',
+    cookieKey:'wifinanadoudoujack12345678!'
+}
