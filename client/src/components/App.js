3a7c405702840a39ce73224b2ffb38e24b3d6a25
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2024-07-10 15:08:54 +1000

    Develop Header component

---
+++ client/src/components/App.js
@@ -0,0 +1,49 @@
+import React, {Component} from "react";
+import {BrowserRouter, Route} from "react-router-dom";
+import {connect} from 'react-redux';
+import * as actions from '../actions';
+
+import Header from "./Header";
+
+function Dashboard() {
+    return (
+        <h2>Dashboard</h2>
+    );
+};
+
+function SurveyNew() {
+    return (
+        <h2>SurveyNew</h2>
+    );
+};
+
+function Landing() {
+    return (
+        <h2>Landing</h2>
+    );
+};
+
+
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+
+    render() {
+        return (
+            <div className="container">
+                {/*  <a href="/auth/google">Sign in with Google</a>*/}
+                <BrowserRouter>
+                    <div>
+                        <Header/>
+                        <Route exact path='/' component={Landing}/>
+                        <Route exact path='/surveys' component={Dashboard}/>
+                        <Route path='/surveys/new' component={SurveyNew}/>
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
+}
+
+export default connect(null, actions)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,21 @@
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from "redux";
+import {thunk} from 'redux-thunk';
+
+import App from './components/App';
+import reducers from "./reducers";
+
 import reportWebVitals from './reportWebVitals';
+
+const store = createStore(reducers,{}, applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <Provider store={store}><App /></Provider>
   </React.StrictMode>
 );
 
