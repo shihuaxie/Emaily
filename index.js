80888a75a9512ab6256e14a8bd100a84621d18e0
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2024-07-27 13:35:55 +1000

    Added billing and client side app

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ app.use(bodyParser.json());
 
 app.use(
     cookieSession({
-        maxAge: 30*24*60*60*1000,
-        keys:[keys.cookieKey]
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
     })
 );
 
@@ -30,6 +30,19 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    //express will serve up production assets
+    //like our main.js file, or main.css file
+    app.use(express.static('client/build'));
+
+    //express will serve up the index.html file
+    //if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 
-app .listen(PORT);
+app.listen(PORT);
